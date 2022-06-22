d2c72d7f409f4b40d844da35bfcb82e8b90b6223
{193-M} <{morokiik@gmail.com}>
2022-06-22 14:22:45 +0900

    test

--- register.php
+++ register.php
@@ -1,12 +1,13 @@
 <?PHP
-$message = '接続に成功しました';
+$message = '';
 try {
   $DBSERVER = 'localhost';
   $DBNAME = 'board';
   $DBUSER = 'board'; //作成したユーザー名
   $DBPASSWD = 'boardpw'; //作成したユーザーのパスワード
   $dsn = "mysql:host={$DBSERVER};dbname={$DBNAME};charset=utf8";
-  $pdo = new \PDO($dsn, $DBUSER, $DBPASSWD, array(\PDO::ATTR_EMULATE_PREPARES => false));   
+  $pdo = new \PDO($dsn, $DBUSER, $DBPASSWD, array(\PDO::ATTR_EMULATE_PREPARES => false));
+  $message="接続に成功しました:{$e->getMessage()}";
 } catch (Exception $e){
   $message="接続に失敗しました:{$e->getMessage()}";
 }
