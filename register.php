e1cc9f782407e92b5a477ff9eada074963b00d08
{193-M} <{morokiik@gmail.com}>
2022-06-22 14:51:16 +0900

    register

--- register.php
+++ register.php
@@ -37,30 +37,30 @@ if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['name
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <title>新規作成</title>
-</head>
-<body>
-<header>
+  </head>
+  <body>
+    <header>
+      <div>
+        <a href="/vantan-board/index.php">TOP</a>
+        <a href="/vantan-board/register.php">新規作成</a>
+        <a href="/vantan-board/login.php">ログイン</a>
+        <a href="/vantan-board/logout.php">ログアウト</a>
+      </div>
+      <h1>新規作成</h1>
+    </header>
     <div>
-        <a href="/vantan_board/index.php">TOP</a>
-        <a href="/vantan_board/register.php">新規作成</a>
-        <a href="/vantan_board/login.php">ログイン</a>
-        <a href="/vantan_board/logout.php">ログアウト</a>
-    </div>
-    <h1>新規作成</h1>
-</header>
-<div>
-    <div style="color: red">
+      <div style="color: red">
         <?php echo $message; ?>
-    </div>
-    <form action="/vantan_board/register.php" method="post">
+      </div>
+      <form action="/vantan-board/register.php" method="post">
         <label>メールアドレス: <input type="email" name="email"/></label><br/>
         <label>パスワード: <input type="password" name="password"/></label><br/>
         <label>名前: <input type="text" name="name"/></label><br/>
         <input type="submit" value="新規登録">
-    </form>
-</div>
-</body>
+      </form>
+      </div>
+  </body>
 </html>
