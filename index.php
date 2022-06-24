08c4a46580a5395917638b6bb09f875e6489150e
{193-M} <{morokiik@gmail.com}>
2022-06-24 10:54:47 +0900

    index

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>トップ</title>
-    </head>
-    <body>
-        <header>
-            <div>
-                <a href="/board/index.php">TOP</a>
-                <a href="/board/register.php">新規作成</a>
-                <a href="/board/login.php">ログイン</a>
-                <a href="/board/logout.php">ログアウト</a>
-            </div>
-            <h1>トップ</h1>
-        </header>
-        <div>
-            <?php echo "{$_SESSION['name']}さんようこそ"; ?>
-        </div>
-    </body>
+  <head>
+    <meta charset="UTF-8">
+    <title>トップ</title>
+  </head>
+  <body>
+    <header>
+      <div>
+        <a href="/vantan-board/index.php">TOP</a>
+        <a href="/vantan-board/register.php">新規作成</a>
+        <a href="/vantan-board/login.php">ログイン</a>
+        <a href="/vantan-board/logout.php">ログアウト</a>
+      </div>
+      <h1>トップ</h1>
+    </header>
+    <div>
+        <?php echo "{$_SESSION['name']}さんようこそ"; ?>
+    </div>
+  </body>
 </html>
