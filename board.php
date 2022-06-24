1969880051573d0b5f1f5db3001e6eb0bb42c0a6
{193-M} <{morokiik@gmail.com}>
2022-06-24 14:50:01 +0900

    board

--- board.php
+++ board.php
@@ -21,8 +21,7 @@
     $pdo = new PDO($dsn, $DBUSER, $DBPASSWD, array(PDO::ATTR_EMULATE_PREPARES => false));
   } catch (Exception $e) {
     $message = "接続に失敗しました: {$e->getMessage()}";
-    
-    
+  }
 
     $sql = 'SELECT * FROM `boards` WHERE id = :id';
     $stmt = $pdo->prepare($sql);
@@ -33,30 +32,62 @@
     if(empty($board)){
         header('Location: /vantan-board/index.php');
         exit;
-    }  
+    } 
+
+    if(!empty($_POST['comment']) && !empty($_SESSION['id'])) {
+        $sql = 'INSERT INTO `comments` (boardId, userId, comment, created)';
+        $sql .= ' VALUES (:boardId, :userId, :comment, NOW())';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':boardId', $id, \PDO::PARAM_INT);
+        $stmt->bindValue(':userId', $_SESSION['id'], \PDO::PARAM_INT);
+        $stmt->bindValue(':comment', $_POST['comment'], \PDO::PARAM_STR);
+        $result = $stmt->execute();
+        if($result) {
+    
+        } else {
+            $message = 'コメントに失敗しました';
+        }
+    }
+   
 
 ?>
 
 <!DOCTYPE html>
- <html lang="en">
- <head>
+<html lang="en">
+  <head>
    <meta charset="UTF-8">
    <title>掲示板作成</title>
- </head>
- <body>
- <header>
-   <div>
-     <a href="/board/index.php">TOP</a>
-     <a href="/board/create_board.php">掲示板作成</a>
-     <a href="/board/register.php">新規作成</a>
-     <a href="/board/login.php">ログイン</a>
-     <a href="/board/logout.php">ログアウト</a>
-   </div>
-   <h1><?php echo $board['title']; ?></h1>
- </header>
- <div>
-   <div style="color: red">
-       <?php echo $message; ?>
-   </div>
-   <div>
-     コメント一覧
+  </head>
+  <body>
+    <header>
+      <div>
+        <a href="/board/index.php">TOP</a>
+        <a href="/board/create_board.php">掲示板作成</a>
+        <a href="/board/register.php">新規作成</a>
+        <a href="/board/login.php">ログイン</a>
+        <a href="/board/logout.php">ログアウト</a>
+      </div>
+      <h1><?php echo $board['title']; ?></h1>
+    </header>
+    <div>
+      <div style="color: red">
+        <?php echo $message; ?>
+      </div>
+      <div>
+        コメント一覧
+        <ul>
+          <?php
+            foreach ($commentList as $comment) {
+              echo "<li>{$comment['comment']}</li>";
+            }
+          ?>
+        </ul>
+      </div>
+      <form action="board.php?id=<?php echo $id; ?>" method="post">
+        <label>コメント: <input type="text" name="comment"/></label><br/>
+        <input type="submit" value="コメント">
+      </form>
+    </div>
+  </body>
+</html>
+
